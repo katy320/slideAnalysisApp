568c3b5b5e8e962b8e55c0964d472af0ce592577
Daniel Perlman <dmp359@drexel.edu>
2019-03-18 13:16:39 -0400

    fullscreen app

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+// From https://github.com/mikeal/vuejs-electron-demo/blob/master/app.js
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 let url
 if (process.env.NODE_ENV === 'DEV') {
-  url = 'http://localhost:8080/'
+  url = 'http://localhost:8080/';
 } else {
-  url = `file://${process.cwd()}/dist/index.html`
+  url = `file://${process.cwd()}/dist/index.html`;
 }
 
 app.on('ready', () => {
-  let window = new BrowserWindow({width: 800, height: 600})
-
-  window.loadURL(url)
+  let window = new BrowserWindow();
+  window.setFullScreen(true);
+  window.loadURL(url);
 });
